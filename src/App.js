2fe13d2de1758e167f38a624c0b03435d48f85b8
Wallison Henrique <wallisonhen@gmail.com>
2020-09-15 15:54:03 -0300

    Implementar component root

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import GlobalStyles from './styles/global';
+
+import Search from './components/Search/Search';
+import Users from './components/Users/Users';
+import Pagination from './components/Pagination/Pagination';
+
+import useGetData from './utils/useGetData';
+import Load from './components/Load/Load';
 
 export default function App() {
-  return <>
-    Sou o App.
-  </>
+	const [ data, setData ] = useGetData();
+
+	const [ loading, setLoading ] = useState(true);
+
+	const [ params, setParams ] = useState({ page: 1, size: 50 });
+
+	const getData = setData(setLoading);
+	
+	useEffect(() => {
+		getData(params);
+	}, [ params.page, params.size, params.name, params.age ]);
+
+	const search = (terms) => setParams({ ...params, ...terms });
+
+	return <div className="App">
+		<GlobalStyles />
+		<Search search={ search } />
+		<Pagination 
+			currentPage={ data.currentPage } 
+			totalPages={ data.totalPages } 
+			search={ search }
+			currentSize={ data.size}
+		/>
+		<Users users={ data.users } />
+		<Pagination 
+			currentPage={ data.currentPage } 
+			totalPages={ data.totalPages } 
+			search={ search }
+			currentSize={ data.size}
+		/>
+		{ loading && <Load /> }
+	</div>
 }
