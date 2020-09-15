f5796dfaf50f1fcd50b805f08066dc0c0c601278
Wallison Henrique <wallisonhen@gmail.com>
2020-09-15 16:13:12 -0300

    Updates

---
+++ precache-manifest.897c45d44c0594e27e7a3783bbce8c85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e338efaa4ea58b2f2e9e8e7557e61f",
+    "url": "/seven-apps/index.html"
+  },
+  {
+    "revision": "f33c2b65fd1c81829206",
+    "url": "/seven-apps/static/js/2.9f331f60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/seven-apps/static/js/2.9f331f60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb0c7ac97ee53139e12",
+    "url": "/seven-apps/static/js/main.b03cc3b5.chunk.js"
+  },
+  {
+    "revision": "b2189057fe9111fcc2f3",
+    "url": "/seven-apps/static/js/runtime-main.c7a5c5d6.js"
+  }
+]);
